c7fb53d5f5c0b5a797a7936758c979713e16d837
João Vitor Silveira Ribeiro <joaovitorsrs7@gmail.com>
2022-07-21 13:13:07 -0300

    New parse_args method, that handles the args.

---
+++ src/args.rs
@@ -0,0 +1,32 @@
+pub struct Args {
+    pub note: String,
+    pub remove: bool,
+}
+
+pub fn parse_args() -> Result<Args, lexopt::Error> {
+    use lexopt::prelude::*;
+
+    let mut note = None;
+    let mut remove = false;
+    let mut parser = lexopt::Parser::from_env();
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Short('r') | Long("remove") => {
+                remove = true;
+            }
+            Value(val) if note.is_none() => {
+                note = Some(val.into_string()?);
+            }
+            Short('h') | Long("help") => {
+                println!("Usage: tnt [-r|--remove] NOTE");
+                std::process::exit(0);
+            }
+            _ => return Err(arg.unexpected()),
+        }
+    }
+
+    Ok(Args {
+        note: note.ok_or("missing argument NOTE")?,
+        remove,
+    })
+}
